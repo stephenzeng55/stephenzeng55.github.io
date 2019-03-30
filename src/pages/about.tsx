20b1b9f2916ab5da7d3666cfa970f67141318800
Stephen Z <stephen.zeng55@gmail.com>
2019-03-29 23:14:21 -0700

    Updates

--- src/pages/about.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import Layout from "../components/layout"
-
-export default () => (
-  <Layout>
-    <h1>Who am I?</h1>
-    <p>Beantown born and raised.</p>
-  </Layout>
-)
