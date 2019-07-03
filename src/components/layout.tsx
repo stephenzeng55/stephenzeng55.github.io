98b205905725dd8b402c4dd7434cc26407ce3f50
Stephen Zeng <stephen.zeng55@gmail.com>
2019-07-03 00:17:14 -0700

    Fixing Links

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import styles from "./layout.module.scss"
 import logo from "../images/icon.png"
@@ -10,7 +9,7 @@ import IconButton from "@material-ui/core/IconButton"
 import Button from "@material-ui/core/Button"
 import { ThemeProvider } from "@material-ui/styles"
 import theme from "../theme"
-import { FormattedMessage, injectIntl } from "gatsby-plugin-intl"
+import { Link, FormattedMessage, injectIntl } from "gatsby-plugin-intl"
 import Language from "./language"
 
 const NavLink = props => (
