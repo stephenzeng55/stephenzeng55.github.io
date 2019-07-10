5e0add49d186dc4167b7c6f710b092ed577e8973
Stephen Zeng <stephen.zeng55@gmail.com>
2019-07-09 22:59:28 -0700

    Added localization for about page.

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -14,8 +14,42 @@ export default () => (
     >
       <FormattedMessage id="who_am_i" />
     </Typography>
-    <Typography variant="h5" color="textPrimary">
-      <FormattedMessage id="blurb" />
+    <Typography
+      variant="body1"
+      color="textPrimary"
+      style={{ width: `75%`, margin: `0 auto` }}
+    >
+      <FormattedMessage id="who_am_i_blurb" />
+    </Typography>
+    <Typography
+      variant="h2"
+      color="textPrimary"
+      gutterBottom
+      className={pageStyles.topTypography}
+    >
+      <FormattedMessage id="what_do_i_do" />
+    </Typography>
+    <Typography
+      variant="body1"
+      color="textPrimary"
+      style={{ width: `75%`, margin: `0 auto` }}
+    >
+      <FormattedMessage id="what_do_i_do_blurb" />
+    </Typography>
+    <Typography
+      variant="h2"
+      color="textPrimary"
+      gutterBottom
+      className={pageStyles.topTypography}
+    >
+      <FormattedMessage id="what_do_i_do_fun" />
+    </Typography>
+    <Typography
+      variant="body1"
+      color="textPrimary"
+      style={{ width: `75%`, margin: `0 auto` }}
+    >
+      <FormattedMessage id="what_do_i_do_fun_blurb" />
     </Typography>
   </Layout>
 )
